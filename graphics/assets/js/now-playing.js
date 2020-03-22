7a06627422a9a9f0332e9477bb473bcf2923bba3
Jonathan Kingsley <jon@mirrorweb.com>
2020-03-22 21:53:07 +0000

    Fixed lastfm, somehow

--- graphics/assets/js/now-playing.js
+++ graphics/assets/js/now-playing.js
@@ -1,28 +1,3 @@
-function is_json(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-
-/*
-const http = new XMLHttpRequest();
-
-http.onreadystatechange = (e) => {
-    response = http.responseText;
-    console.log(http);
-    if (is_json(response)) {
-        response_json = JSON.parse(response);
-        console.log(response_json);
-        var recent_track = response_json.recenttracks.track[0];
-        console.log(recent_track);
-        set_now_playing(recent_track);
-    }
-}
-*/
-
 const lastfm_data = {
     baseURL: "https://ws.audioscrobbler.com/2.0/",
     method: "user.getrecenttracks",
@@ -34,70 +9,22 @@ const lastfm_data = {
 var now_playing = null;
 
 function set_now_playing(val) {
-    if (now_playing === null || now_playing.name !== val.name || now_playing.artist !== val.artist) {
+    if (now_playing === null || now_playing.name !== val.name || now_playing.artist['#text'] !== val.artist['#text']) {
         now_playing = val;
 
-        show_now_playing(now_playing.name, now_playing.artist, 5000);
+        show_now_playing(now_playing.name, now_playing.artist['#text'], 5000);
     }
 }
 
 function get_lastfm() {
-    url = sprintf("%s?method=%s&user=%s&api_key=%s%s", lastfm_data.baseURL, lastfm_data.method, lastfm_data.user, lastfm_data.api_key, lastfm_data.additional);
-    console.log(url);
-    fetch(url).then(function(response) {
-        console.log(response);
+    fetch(lastfm_data.baseURL + "?method=" + lastfm_data.method + "&user=" + lastfm_data.user + "&api_key=" + lastfm_data.api_key + lastfm_data.additional).then(function(response) {
+        return response.json();
     }).then(function(data) {
-        console.log(data);
+        set_now_playing(data.recenttracks.track[0])
     });
 }
 
-/*function get_lastfm_old() {
-    $.ajax({
-        type: "GET",
-        url:
-            lastfm_data.baseURL +
-            lastfm_data.user +
-            "&api_key=" +
-            lastfm_data.api_key +
-            lastfm_data.additional,
-        dataType: "json",
-        success: function(resp) {
-            console.log("Success!");
-            var recentTrack = resp.recenttracks.track[0];
-            console.log(recentTrack);
-            var formatted =
-                "<img src='https://i.imgur.com/EgWjJry.png'>" + recentTrack.name;
-            $("a#tracktitle")
-                .html(formatted)
-                .attr("href", recentTrack.url)
-                .attr("title", recentTrack.name + " by " + recentTrack.artist["#text"])
-                .attr("target", "_blank");
-
-            var artistFormatted =
-                "<img src='https://i.imgur.com/fae5XZA.png'>" +
-                recentTrack.artist["#text"];
-            $("a#trackartist")
-                .html(artistFormatted)
-                .attr("title", "Artist : " + recentTrack.artist["#text"]);
-            $("img#trackart").attr("src", recentTrack.image[2]["#text"]);
-        },
-        error: function(resp) {
-            console.log("Error!");
-            console.log(resp);
-            $("a#tracktitle").html(
-                "<img src='https://i.imgur.com/EgWjJry.png'>" + "Silence!"
-            );
-            $("img#trackart").attr("src", "https://i.imgur.com/Q6cCswP.jpg");
-            var artistFormatted =
-                "<img src='https://i.imgur.com/fae5XZA.png'>Prashant Shrestha";
-            $("a#trackartist")
-                .html(artistFormatted)
-                .attr("href", "www.prashant.me/");
-        }
-    });
-};*/
-
 // Get the new one.
-//getSetLastFM();
+get_lastfm();
 // Start the countdown.
-//setInterval(getSetLastFM, 10 * 1000);
+setInterval(get_lastfm, 5 * 1000);
